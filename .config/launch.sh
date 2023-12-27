6ce2fc68d1c7431b0ae06bbcc56ba97528794e9d
Dani <sergiochogamez28@hotmail.com>
2023-12-27 13:43:57 -0600

    Reestructuracion polybar

--- .config/launch.sh
+++ .config/launch.sh
@@ -10,4 +10,6 @@ echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 polybar my_bar 2>&1 | tee -a /tmp/polybar1.log & disown
 # polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
 
+
 echo "Bars launched..."
+
